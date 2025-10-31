51e11c48e33d32593493441497e866f47163d98e
doochineroo <dooheeee1209@gmail.com>
2025-10-31 14:32:28 +0900

    필요없는 파일 삭제

--- add_restaurant_photos.sql
+++
@@ -1,7 +0,0 @@
--- 매장 사진 5개 컬럼 추가
-ALTER TABLE restaurants ADD COLUMN restaurant_photo1 VARCHAR(500) NULL AFTER main_image;
-ALTER TABLE restaurants ADD COLUMN restaurant_photo2 VARCHAR(500) NULL AFTER restaurant_photo1;
-ALTER TABLE restaurants ADD COLUMN restaurant_photo3 VARCHAR(500) NULL AFTER restaurant_photo2;
-ALTER TABLE restaurants ADD COLUMN restaurant_photo4 VARCHAR(500) NULL AFTER restaurant_photo3;
-ALTER TABLE restaurants ADD COLUMN restaurant_photo5 VARCHAR(500) NULL AFTER restaurant_photo4;
-
